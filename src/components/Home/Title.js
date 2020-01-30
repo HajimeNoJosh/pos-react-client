7c984787203fe6b82b2774c9fb7062e7d2122096
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-01-29 21:51:07 -0500

    change navigation for home

--- src/components/Home/Title.js
+++ src/components/Home/Title.js
@@ -4,7 +4,7 @@ const Title = (props) => {
   const { title, subtitle, size } = props
   return (
     <div className={'title col-sm-' + size}>
-      <h2 className='joining'><a className='title-color' href='/#'>{title}</a></h2>
+      <h2 className='joining'><a className='title-color' href='/#/'>{title}</a></h2>
       <p className='joining'>{subtitle}</p>
     </div>
   )
